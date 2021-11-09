f94bff1609c6e4e3aa83483d5c7b08d31e4fbed8
Brian Diaz <bidiaz101@gmail.com>
2021-11-09 08:13:42 -0600

    Completed assignment

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,18 +1,27 @@
 function fibonacci(num) {
   // type your code here
+  const seed = [0, 1]
+
+  for(let i = 0 ; i < num ; i++) {
+    seed.push(seed[i] + seed[i + 1])
+  }
+
+  return seed[num]
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 0");
+  console.log("Expecting 0");
   console.log("=>", fibonacci(0));
 
-  console.log("");
+  console.log("Expecting 6765");
+  console.log("=>", fibonacci(20))
 
-  console.log("Expecting: 1");
+  console.log("Expecting 1");
   console.log("=>", fibonacci(2));
 
-  console.log("");
+  console.log("Expecting 610")
+  console.log("=>", fibonacci(15));
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
@@ -21,4 +30,9 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+
+// start sequence with array with 0 and 1
+// loop through array and use num to do looping however many times
+// return value after loop has finished
+
 // And a written explanation of your solution
--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,17 +1,30 @@
 def fibonacci(num)
   # type your code in here
+  seeds = [0, 1]
+
+  i = 0
+
+  num.times do 
+    seeds.push(seeds[i] + seeds[i + 1])
+
+    i += 1
+  end
+
+  seeds[num]
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 0"
   puts "=>", fibonacci(0)
 
-  puts
+  puts "Expecting: 610"
+  puts "=>", fibonacci(15)
 
   puts "Expecting: 1"
   puts "=>", fibonacci(2)
 
-  puts
+  puts "Expecting: 6765"
+  puts "=>", fibonacci(20)
 
   puts "Expecting: 55"
   puts "=>", fibonacci(10)
